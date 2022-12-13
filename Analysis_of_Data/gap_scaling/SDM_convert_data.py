affc5a14ac08e2db38a1332f5e9c2179b598db7f
DaRos97 <dario.rossim@gmail.com>
2022-12-13 09:43:15 +0100

    added convert_data for SDM-type diagrams

--- Analysis_of_Data/gap_scaling/SDM_convert_data.py
+++ Analysis_of_Data/gap_scaling/SDM_convert_data.py
@@ -11,36 +11,20 @@ import csv
 #In order to work, final_S_DM has to contain all the ansatze, so that then we can compare it in the phase diagram using effort_DM/plot.py
 cutoff_gap = 1e-6
 #parameters are: ansatz, j2,j3, DM angle, Spin
-list_ans = ['3x3','q0','cb1','cb1_nc','cb2','oct']
-DM_list = {'000':0, '005':0.05, '104':np.pi/3, '209':2*np.pi/3}
-argv = sys.argv[1:]
-try:
-    opts, args = getopt.getopt(argv, "S:", ['DM='])
-    S = 0.5
-    txt_S = '50'
-    DM = '000'
-except:
-    print("Error in input parameters")
-    exit()
-for opt, arg in opts:
-    if opt in ['-S']:
-        txt_S = arg
-        S_label = {'50':0.5,'36':(np.sqrt(3)-1)/2,'34':0.34,'30':0.3,'20':0.2}
-        S = S_label[txt_S]
-    if opt == '--DM':
-        DM = arg.replace('.','')
-        if DM not in DM_list.keys():
-            print('Not computed DM angle')
-            exit()
-        type_of_ans = 'SU2' if DM in ['000','104','209'] else 'TMD'
-N_max = 37
-print("Using arguments: Dm angle = ",DM," spin S = ",S," and cutoff: ",cutoff_gap)
-#import data
-data_dirname = '../../Data/final_'+txt_S+'_'+DM+'/'
+list_ans = ['1a','1b','1c','1f']
+S_max = 0.5
+DM_max = 0.5
+S_pts = 10
+DM_pts = 15
+S_list = np.linspace(0.05,S_max,S_pts,endpoint=True)
+DM_list = np.logspace(-5,np.log(DM_max),DM_pts,base = np.e)
+DM_list_neg = np.flip(-DM_list)#.reverse()
+DM_list_neg = np.append(DM_list_neg,np.array([0]),axis = 0)
+DM_list = np.concatenate((DM_list_neg,DM_list))
+
+data_dirname = '../../Data/SDM/final_SDM/'
 ##### 
-m_ans = {'q0':1, '3x3':3, 'cb1':4, 'cb1_nc':4}
-m_ans_gauge = {'q0':3, '3x3':3, 'cb1':12, 'cb1_nc':12}
-########
+type_of_ans = 'SDM'
 for filename in os.listdir(data_dirname):
     data_name = data_dirname + filename
     with open(data_name, 'r') as f:
@@ -50,33 +34,25 @@ for filename in os.listdir(data_dirname):
     print(filename)
     for i in range(N__):
         data = lines[i*2+1].split(',')
-        data[-1] = data[-1][:-1]
+        data[-1] = data[-1][:-1]                #remove final \n
         header = lines[i*2].split(',')
         header[-1] = header[-1][:-1]
+        S = float(data[1])
+        DM = float(data[2])
         #
         if data[3][0] == 'F' or data[3][-1] in ['L','O']:
             continue
         # Go to data directory and compute gap scaling up to this point
         ans = data[0]
-        J2 = float(data[1])
-        J3 = float(data[2])
-        if DM != '209':
-            m_ = m_ans[ans]
-        else:
-            m_ = m_ans_gauge[ans]
-        N_reference = np.arange(13,N_max - (N_max-13)%m_ + m_,m_,dtype = int)
-        NN_n_ = []
-        for n in N_reference:
-            if n>25 and n<37:
-                continue
-            NN_n_.append(n)
-        N_reference = NN_n_
+        N_reference = [13,25,37]
         #list directories to find list of Ns
-        Data_dir = '../../Data/S'+txt_S+'/phi'+DM+'/'
+        Data_dir = '../../Data/SDM/'
         Ns = []
         params = {}
         #get all parameters at all Ns
         for N_dir in os.listdir(Data_dir):
+            if N_dir == 'final_SDM':
+                continue
             if int(N_dir) not in N_reference:
                 continue
             for files in os.listdir(Data_dir+N_dir+'/'):
@@ -85,7 +61,7 @@ for filename in os.listdir(data_dirname):
                 N_ = (len(lines_1)-1)//2 +1
                 for i_ in range(N_):
                     lines_2 = lines_1[i_*2+1].split(',')
-                    if lines_2[0] == ans and np.abs(float(lines_2[1])-J2) < 1e-6 and np.abs(float(lines_2[2])-J3) < 1e-6 and lines_2[3][0] == 'T':
+                    if lines_2[0] == ans and np.abs(float(lines_2[1])-S) < 1e-6 and np.abs(float(lines_2[2])-DM) < 1e-6 and lines_2[3][0] == 'T':
                         Ns.append(int(N_dir))
                         params[N_dir] = []
                         for p in lines_2[6:]:
@@ -94,17 +70,19 @@ for filename in os.listdir(data_dirname):
         gaps = []
         N_N = np.sort(Ns)
         for nnn_ in N_N:
-            gaps.append(fs.find_gap(params[str(nnn_)],nnn_,[ans,DM_list[DM],J2,J3,txt_S,type_of_ans]))
+            DM_val = DM_list[list(DM_list).index(DM)]
+            gaps.append(fs.find_gap(params[str(nnn_)],nnn_,[ans,DM_val,0,0,'0',type_of_ans]))
+
         try:
             parameters, covariance = curve_fit(fs.linear, N_N, gaps, p0=(1,0))
         except:
-            print("Not fitted ",ans,"at ",J2,J3)
+            print("Not fitted ",ans," at ",S,DM)
             fitted_ = fs.linear(np.linspace(N_N[0],N_N[-1],100),parameters[0],parameters[1])
             plt.figure()
             plt.plot(N_N,gaps,'ro')
             plt.plot(np.linspace(N_N[0],N_N[-1],100),fitted_,'g-')
             plt.hlines(parameters[1],N_N[0],N_N[-1],color='green',linestyles='--')
-            plt.title(ans+"__"+str(J2)+"_"+str(J3))
+            plt.title(ans+"__"+str(S)+"_"+str(DM))
             plt.show()
         #Change True -> TrueL or TrueO
         order = 'L' if parameters[1] > cutoff_gap else 'O'
--- Analysis_of_Data/gap_scaling/functions.py
+++ Analysis_of_Data/gap_scaling/functions.py
@@ -44,8 +44,8 @@ def find_gap(data,N,arguments):
         if data[2+p_]:
             P.append(data[2+p_])
     m = 6
-    is_SU2 = arguments[-1]
-    matrix = matrix_SU2 if is_SU2 else matrix_TMD
+    funcs = {'SU2': matrix_SU2, 'TMD': matrix_TMD, 'SDM': matrix_SDM}
+    matrix = funcs[arguments[-1]]
     Nk = matrix(P,N,arguments,data[1],kkg)
     min_energies = np.zeros((N,N))
     J_ = np.zeros((2*m,2*m))
@@ -384,6 +384,163 @@ def matrix_SU2(P,N,arguments,L,kkg):
     ################
     #print(A1,A2,A3,B1,B2,B3,phiA1p,phiA2,phiA2p,phiA3,phiB1,phiB1p,phiB2,phiB2p,phiB3)
     N = np.zeros((2*m,2*m,N,N), dtype=complex)
+    ##################################### B
+    b1 = B1*np.exp(1j*phiB1);               b1_ = np.conjugate(b1)
+    b1p = B1*np.exp(1j*phiB1p);             b1p_ = np.conjugate(b1p)
+    b1pi = B1*np.exp(1j*(phiB1p+p1*np.pi)); b1pi_ = np.conjugate(b1pi)
+    b2 = B2*np.exp(1j*phiB2);               b2_ = np.conjugate(b2)
+    b2i = B2*np.exp(1j*(phiB2+p1*np.pi));   b2i_ = np.conjugate(b2i)
+    b2p = B2*np.exp(1j*phiB2p);             b2p_ = np.conjugate(b2p)
+    b2pi = B2*np.exp(1j*(phiB2p+p1*np.pi)); b2pi_ = np.conjugate(b2pi)
+    b3 = B3*np.exp(1j*phiB3);               b3_ = np.conjugate(b3)
+    b3i = B3*np.exp(1j*(phiB3+p1*np.pi));   b3i_ = np.conjugate(b3i)
+    #
+    N[0,1] = J1*b1p_ *ka1  *t1_              + J2*b2*t2                         #t1
+    N[0,2] = J1*b1p        *t1               + J2*b2p_ *ka1*t2                  #t1_
+    N[0,4] = J1*b1_  *ka2_ *t1               + J2*b2pi *ka12m*t2_               #t1
+    N[0,5] = J1*b1   *ka2_ *t1_              + J2*b2i_ *ka12p_*t2_              #t1_
+    N[1,2] = J1*(b1_       *t1  + b1p_*ka1_*t1_)                                #t1     t1
+    N[1,3] = J1*b1         *t1_              + J2*b2p_ *ka1_*t2                 #t1_
+    N[1,5] =                                   J2*(b2  *ka12p_*t2 + b2p*t2_)
+    N[2,3] = J1*b1_        *t1               + J2*b2   *ka1*t2                  #t1
+    N[2,4] =                                   J2*(b2p_*ka2_*t2 + b2i_*ka1*t2_)
+    N[3,4] = J1*b1pi_*ka1  *t1_              + J2*b2*t2                         #t1
+    N[3,5] = J1*b1p        *t1               + J2*b2pi_*ka1*t2                  #t1_
+    N[4,5] = J1*(b1_       *t1  + b1pi_*ka1_*t1_)                               #t1     t1
+
+    N[0,0] = J3*b3i_ *ka1_ *t3_
+    N[3,3] = J3*b3_  *ka1_ *t3_
+    N[1,4] = J3*(b3_ *ka2_ *t3_ + b3       *t3)
+    N[2,5] = J3*(b3  *ka12p_  *t3  + b3i_*ka1*t3_)
+    ####other half square                                                       #Same ts
+    N[m+0,m+1] = J1*b1p  *ka1  *t1_           + J2*b2_*t2
+    N[m+0,m+2] = J1*b1p_       *t1            + J2*b2p  *ka1*t2
+    N[m+0,m+4] = J1*b1   *ka2_ *t1            + J2*b2pi_*ka12m*t2_
+    N[m+0,m+5] = J1*b1_  *ka2_ *t1_           + J2*b2i  *ka12p_*t2_
+    N[m+1,m+2] = J1*(b1        *t1  + b1p*ka1_*t1_)
+    N[m+1,m+3] = J1*b1_        *t1_           + J2*b2p  *ka1_*t2
+    N[m+1,m+5] =                                J2*(b2_ *ka12p_*t2 + b2p_*t2_)
+    N[m+2,m+3] = J1*b1         *t1            + J2*b2_  *ka1*t2
+    N[m+2,m+4] =                                J2*(b2p *ka2_*t2 + b2i *ka1*t2_)
+    N[m+3,m+4] = J1*b1pi *ka1  *t1_           + J2*b2_*t2
+    N[m+3,m+5] = J1*b1p_       *t1            + J2*b2pi *ka1*t2
+    N[m+4,m+5] = J1*(b1        *t1  + b1pi*ka1_*t1_)
+
+    N[m+0,m+0] = J3*b3i *ka1_ *t3_
+    N[m+3,m+3] = J3*b3  *ka1_ *t3_
+    N[m+1,m+4] = J3*(b3 *ka2_ *t3_ + b3_  *t3)
+    N[m+2,m+5] = J3*(b3_*ka12p_  *t3  + b3i *ka1*t3_)
+    ######################################## A
+    a1 =    A1
+    a1p =   A1*np.exp(1j*phiA1p)
+    a1pi =  A1*np.exp(1j*(phiA1p+p1*np.pi))
+    a2 =    A2*np.exp(1j*phiA2)
+    a2i =   A2*np.exp(1j*(phiA2+p1*np.pi))
+    a2p =   A2*np.exp(1j*phiA2p)
+    a2pi =  A2*np.exp(1j*(phiA2p+p1*np.pi))
+    a3 =    A3*np.exp(1j*phiA3)
+    a3i =   A3*np.exp(1j*(phiA3+p1*np.pi))
+    N[0,m+1] = - J1*a1p *ka1 *t1_           +J2*a2*t2
+    N[0,m+2] =   J1*a1p      *t1            -J2*a2p  *ka1*t2
+    N[0,m+4] = - J1*a1  *ka2_*t1            +J2*a2pi *ka12m*t2_
+    N[0,m+5] =   J1*a1  *ka2_*t1_           -J2*a2i  *ka12p_*t2_
+    N[1,m+2] = - J1*(a1      *t1   +a1p*ka1_*t1_)
+    N[1,m+3] =   J1*a1       *t1_           -J2*a2p  *ka1_*t2
+    N[1,m+5] =                               J2*(a2  *ka12p_*t2  +a2p*t2_)
+    N[2,m+3] = - J1*a1       *t1            +J2*a2   *ka1*t2
+    N[2,m+4] =                              -J2*(a2p *ka2_*t2  +a2i*ka1*t2_)
+    N[3,m+4] = - J1*a1pi*ka1 *t1_           +J2*a2*t2
+    N[3,m+5] =   J1*a1p      *t1            -J2*a2pi *ka1*t2
+    N[4,m+5] = - J1*(a1      *t1   +a1pi*ka1_*t1_)
+
+    N[0,m+0] = - J3*a3i *ka1_*t3_
+    N[3,m+3] = - J3*a3  *ka1_*t3_
+    N[1,m+4] = - J3*(a3 *ka2_*t3_  -a3 *t3)
+    N[2,m+5] = - J3*(a3i*ka1*t3_  -a3 *ka12p_ *t3)
+    #not the diagonal
+    N[1,m]   =   J1*a1p *ka1_*t1            -J2*a2*t2_
+    N[2,m]   = - J1*a1p      *t1_           +J2*a2p  *ka1_*t2_
+    N[4,m]   =   J1*a1  *ka2 *t1_           -J2*a2pi *ka12m_*t2
+    N[5,m]   = - J1*a1  *ka2 *t1            +J2*a2i  *ka12p*t2
+    N[2,m+1] =   J1*(a1      *t1_  +a1p*ka1 *t1)
+    N[3,m+1] = - J1*a1       *t1            +J2*a2p  *ka1_*t2_
+    N[5,m+1] =                              -J2*(a2  *ka12p*t2_   +a2p*t2)
+    N[3,m+2] =   J1*a1       *t1_           -J2*a2   *ka1_*t2_
+    N[4,m+2] =                               J2*(a2p *ka2*t2_   +a2i*ka1_*t2)
+    N[4,m+3] =   J1*a1pi*ka1_*t1            -J2*a2*t2_
+    N[5,m+3] = - J1*a1p      *t1_           +J2*a2pi *ka1_*t2_
+    N[5,m+4] =   J1*(a1      *t1_  +a1pi*ka1 *t1)
+
+    N[0,m+0] +=  J3*a3i *ka1  *t3
+    N[3,m+3] +=  J3*a3  *ka1  *t3
+    N[4,m+1] =   J3*(a3 *ka2 *t3   -a3 *t3_)
+    N[5,m+2] =   J3*(a3i*ka1_ *t3   -a3 *ka12p *t3_)
+    #################################### HERMITIAN MATRIX
+    #N += np.conjugate(N.transpose((1,0,2,3)))
+    for i in range(2*m):
+        for j in range(i,2*m):
+            N[j,i] += np.conjugate(N[i,j])
+    #################################### L
+    for i in range(2*m):
+        N[i,i] += L
+    return N
+
+
+def matrix_SDM(P,N,arguments,L,kkg):
+    #### vectors of 1nn, 2nn and 3nn
+    a1 = (1,0)
+    a2 = (-1,np.sqrt(3))
+    a12p = (a1[0]+a2[0],a1[1]+a2[1])
+    a12m = (a1[0]-a2[0],a1[1]-a2[1])
+    ans,DM,J2,J3,txt_S,is_SU2 = arguments
+    #### DM
+    DM1 = DM;    DM2 = 0;   DM3 = 2*DM1
+    t1 = np.exp(-1j*DM1);    t1_ = np.conjugate(t1)
+    t2 = np.exp(-1j*DM2);    t2_ = np.conjugate(t2)
+    t3 = np.exp(-1j*DM3);    t3_ = np.conjugate(t3)
+    #### product of lattice vectors with K-matrix
+    ka1 = np.exp(1j*np.tensordot(a1,kkg,axes=1));   ka1_ = np.conjugate(ka1);
+    ka2 = np.exp(1j*np.tensordot(a2,kkg,axes=1));   ka2_ = np.conjugate(ka2);
+    ka12p = np.exp(1j*np.tensordot(a12p,kkg,axes=1));   ka12p_ = np.conjugate(ka12p);
+    ka12m = np.exp(1j*np.tensordot(a12m,kkg,axes=1));   ka12m_ = np.conjugate(ka12m);
+    J1 = 1
+    m = 6
+    J1 /= 2.
+    J2 /= 2.
+    J3 /= 2.
+    j2 = np.sign(int(np.abs(J2)*1e8))   #check if it is 0 or not --> problem with VERY small J2,J3
+    j3 = np.sign(int(np.abs(J3)*1e8))
+    A1 = P[0]
+    B1 = P[1]
+    phiB1 = P[-1]
+    if ans == '1a':      #3
+        phiA1p = 0
+        phiB1p = -phiB1
+        p1 = 0
+    if ans == '1b':      #3
+        phiA1p = np.pi
+        phiB1p = -phiB1
+        p1 = 0
+    if ans[:2] == '1c':      #3
+        phiA1p = 0
+        phiB1p = -phiB1
+        p1 = 1
+    if ans == '1d':      #3
+        phiA1p = np.pi
+        phiB1p = -phiB1
+        p1 = 1
+    if ans == '1e':      #3
+        phiA1p = P[-2]
+        phiB1p = phiB1
+        p1 = 0
+    if ans[:2] == '1f':      #3
+        phiA1p = P[-2]
+        phiB1p = phiB1
+        p1 = 1
+    N = np.zeros((2*m,2*m,N,N), dtype=complex)
+    B2 = 0;B3 = 0;A2 = 0;A3 = 0;
+    phiB2 = 0;phiB3 = 0;phiA2 = 0;phiA3 = 0;
+    phiB2p = 0;phiB3p = 0;phiA2p = 0;phiA3p = 0;
     ##################################### B
     b1 = B1*np.exp(1j*phiB1);               b1_ = np.conjugate(b1)
     b1p = B1*np.exp(1j*phiB1p);             b1p_ = np.conjugate(b1p)
